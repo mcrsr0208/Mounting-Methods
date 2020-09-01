7945b805fa58573253004afaeb75d0dd3f237e6c
mcrsr0208 <mcrsr0208@gmail.com>
2020-09-01 15:20:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+
+class App extends React.Component{
+
+  constructor(props){
+    console.log('constructor()')
+    super(props);
+    this.state = {
+      name:'mcrsr'
+    }
+  }
+ 
+  componentDidMount(){
+    console.log('componentDidMount()');
+  }
+
+
+  static getDerivedStateFromProps(){
+   console.log('static getDerivedStateFromProps()');
+  }
+
+  render(){
+    console.log('render()');
+    return(
+      <div>
+        <h1>{ this.state.name }</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
